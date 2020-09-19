1f56bd0558e94ba90a2e134f1bf8505543838c01
Onur Cem <onurcemsenel@gmail.com>
2020-09-19 22:46:48 +0300

    chore: configure cors

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import bodyParser from 'body-parser';
 
 import collect from './routes/collect';
@@ -7,6 +8,8 @@ const PORT = 8080;
 
 const app = express();
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/collect', collect);
