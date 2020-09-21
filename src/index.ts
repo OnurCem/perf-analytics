d6fb1a211e6c8b885b7b85bfd8c53db8fe563208
Onur Cem <onurcemsenel@gmail.com>
2020-09-22 00:04:49 +0300

    chore: rename port env variable name

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import bodyParser from 'body-parser';
 
 import collect from './routes/collect';
 
-const PORT = process.env.API_PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
